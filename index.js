6b8f9b972fabb18aaefbd311e03af006ae5471b4
matthewmontiel <xoptimuszerox@gmail.com>
2023-04-23 10:59:05 -0500

    fixed branch issue

--- index.js
+++ index.js
@@ -1,60 +1,69 @@
-const {Shape, Triangle, Square, Circle } = require('./lib/shapes.js');
+const { Shape, Triangle, Square, Circle } = require("./lib/shapes.js");
 const fs = require("fs");
 const inquirer = require("inquirer");
 const path = require("path");
 
-
 inquirer
-    .prompt([{
-        type: "checkbox",
-        name: "userShape",
-        message: "What shape would you like to use for your svg?",
-        choices: ["Circle", "Triangle", "Square"],
+  .prompt([
+    {
+      type: "checkbox",
+      name: "userShape",
+      message: "What shape would you like to use for your svg?",
+      choices: ["Circle", "Triangle", "Square"],
     },
-{
-    type: "input",
-    name: "userColor",
-    message: "Choose the color you would like to use for your logo. Please use a color keyword",
-},
-{
-    type: "input",
-    name: "initialsText",
-    message: "Please enter your initials. Do not use more than 3 characters.",
-},
-{
-    type: "input",
-    name: "initialsTextColor",
-    message: "Please enter a color keyword to use as the color for your text.",
-},
-])
-.then((answers) => {
+    {
+      type: "input",
+      name: "userColor",
+      message:
+        "Choose the color you would like to use for your logo. Please use a color keyword",
+    },
+    {
+      type: "input",
+      name: "initialsText",
+      message: "Please enter your initials. Do not use more than 3 characters.",
+    },
+    {
+      type: "input",
+      name: "initialsTextColor",
+      message:
+        "Please enter a color keyword to use as the color for your text.",
+    },
+  ])
+  .then((answers) => {
     let userShape = answers.userShape;
     let userColor = answers.userColor;
-    let userInitials = answers.initialsText;
-    let initialTextColor = answers.initialTextColor;
-    console.log(userShape, userColor, userInitials, initialTextColor);
+    let initialsText = answers.initialsText;
+    let initialsTextColor = answers.initialsTextColor;
+    console.log(userShape, userColor, initialsText, initialsTextColor);
     if (userShape == "Circle") {
-        const circle = new Circle(userColor, userInitials, initialTextColor);
-        const output = circle.makeCircle(circle.userShape, circle.initialsText, circle.initialTextColor);
-        console.log(output);
-        fs.writeFile(`./Output/circle-${newText}.svg`, output, (err) => 
-        err ? console.log(err) : console.log('Circle Created.')
-        );
+      const circle = new Circle(userColor, initialsText, initialsTextColor);
+      const output = circle.makeCircle(
+        circle.color,
+        circle.text,
+        circle.textColor
+      );
+      fs.writeFile(`./Output/circle-${initialsText}.svg`, output, (err) =>
+        err ? console.log(err) : console.log("Circle Created.")
+      );
+    } else if (userShape == "Triangle") {
+      const triangle = new Triangle(userColor, initialsText, initialsTextColor);
+      const output = triangle.makeTriangle(
+        triangle.color,
+        triangle.text,
+        triangle.textColor
+      );
+      fs.writeFile(`./Output/Triangle-${initialsText}.svg`, output, (err) =>
+        err ? console.log(err) : console.log("Triangle Created.")
+      );
+    } else if (userShape == "Square") {
+      const square = new Square(userColor, initialsText, initialsTextColor);
+      const output = square.makeSquare(
+        square.color,
+        square.text,
+        square.textColor
+      );
+      fs.writeFile(`./Output/square-${initialsText}.svg`, output, (err) =>
+        err ? console.log(err) : console.log("Square Created.")
+      );
     }
-    else if (userShape == "Triangle") {
-        const triangle = new Triangle(userColor, userInitials, initialTextColor);
-        const output = triangle.makeTriangle(triangle.userShape, triangle.initialsText, triangle.initialTextColor);
-        console.log(output);
-        fs.writeFile(`./Output/Triangle-${newText}.svg`, output, (err) =>
-        err ? console.log(err) : console.log('Triangle Created.')
-        );
-    }
-    else if (userShape == "Square") {
-        const square = new Square(userColor, userInitials, initialTextColor);
-        const output = triangle.makeSquare(square.userShape, square.initialsText, square.initialTextColor);
-        console.log(output);
-        fs.writeFile(`./Output/square-${newText}.svg`, output, (err) =>
-        err ? console.log(err) : console.log('Square Created.')
-        );
-    }
-});
+  });
